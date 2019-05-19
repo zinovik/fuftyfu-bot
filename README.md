a23ae6a45154f2614bb1e1bf7b521c8cf5317ae9
Max Zinovik <zinovik@gmail.com>
2019-05-19 09:43:37 +0300

    decompose hedgehog part

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-[![Build Status](https://travis-ci.org/zinovik/zinovikbot.svg?branch=master)](https://travis-ci.org/zinovik/zinovikbot)
+[![Build Status](https://travis-ci.org/zinovik/fuftyfu-bot.svg?branch=master)](https://travis-ci.org/zinovik/fuftyfu-bot)
 
 1. .env
 2. docker-compose up
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,13 @@
 process.env['NTBA_FIX_319'] = '1';
 
 import * as dotenv from 'dotenv';
-import * as path from 'path';
 import * as TelegramBot from 'node-telegram-bot-api';
 import * as express from 'express';
 import * as helmet from 'helmet';
-import axios from 'axios';
 
 dotenv.config();
 
 import { handleMessages } from './hedgehog';
-import {
-  schedulerIce,
-  schedulerDvvs,
-  setIceBrestEndpoint,
-  setDvvsBrestEndpoint,
-  addCallback,
-} from './scheduler';
-
-const CURRENT_URL = process.env.CURRENT_URL || 'https://zinovikbot.herokuapp.com';
-const PERIOD = Number(process.env.PERIOD) || 60;
 
 const bot: TelegramBot = new TelegramBot(process.env.TOKEN || '', { polling: true });
 handleMessages(bot);
@@ -28,19 +16,6 @@ const app = express();
 app.use(helmet());
 app.set('port', process.env.PORT || 8100);
 
-setIceBrestEndpoint(app);
-setDvvsBrestEndpoint(app);
-
 app.listen(app.get('port'), () => {
   console.log(`Express server listening on port ${app.get('port')}`);
 });
-
-// Schedules subscriptions
-addCallback(bot);
-schedulerIce(bot);
-schedulerDvvs(bot);
-setInterval(() => schedulerIce(bot), PERIOD * 60 * 1000);
-setInterval(() => schedulerDvvs(bot), PERIOD * 60 * 1000);
-
-// Prevent Heroku Node App From Sleeping
-setInterval(() => axios.get(CURRENT_URL), 15 * 60 * 1000); // every 15 minutes
